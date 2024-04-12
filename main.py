7eb0ed39a5a37d72234573d41eb63b35e530f6ec
Azat Azamat <azatkariuly@Azats-MacBook-Pro-2.local>
2024-04-12 14:16:38 +0900

    test

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import cv2
 import numpy as np
+import pandas as pd
+from PIL import Image
 
 from ultralytics import YOLO
 
@@ -34,30 +36,54 @@ class YOLOv8_Detection:
             #     has_score = True
 
         return img
-    
+
+def make_stats(left_distance='undefined', right_distance='undefined'):
+    bg_width, bg_height = 1500, 500
+    background = np.zeros((bg_height, bg_width, 3), dtype=np.uint8)
+
+    # Define text content and parameters
+    line1 = "Left:  " + str(left_distance)
+    line2 = "Right: " + str(right_distance)
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    font_scale = 4
+    font_thickness = 20
+    font_color = (255, 255, 255)  # White color
+
+    # Add the text to the black background in two lines
+    cv2.putText(background, line1, (200, 180), font, font_scale, font_color, font_thickness)
+    cv2.putText(background, line2, (200, 380), font, font_scale, font_color, font_thickness)
+
+    return background
 
 model_det = YOLOv8_Detection(det_weight)
 
 # Open the video file
-video_path = '../2024-03-13/하이트비전영상/192.168.1.7_01_20240313105731960_5.mp4'
+video_path = '../2024-03-13/하이트비전영상/192.168.1.7_01_20240313105731960_3.mp4'
+data_path = '../2024-03-13/Laser.xlsx'
+
 cap = cv2.VideoCapture(video_path)
+dataframe = pd.read_excel(data_path)
 
 # Check if the video opened successfully
 if not cap.isOpened():
     print("Error: Couldn't open the video file.")
     exit()
 
-# # Get the frame rate of the video
-# fps = cap.get(cv2.CAP_PROP_FPS)
+# Get the frame rate of the video
+fps = int(cap.get(cv2.CAP_PROP_FPS))
 
 # # Calculate the frame number to skip to 1 minute
-# frame_to_skip = int(332 * fps )
+# frame_to_skip = int(39 * fps)
 
 # # Set the frame position to skip to 1 minute
 # cap.set(cv2.CAP_PROP_POS_FRAMES, frame_to_skip)
 
+frame_counter = 1500
+seconds_counter = 0
+
 # Read and display frames until the video ends
 while cap.isOpened():
+
     # Read a frame from the video
     ret, frame = cap.read()
 
@@ -65,6 +91,26 @@ while cap.isOpened():
     if ret:
         # Display the frame
         frame = model_det.detect(frame)
+        frame_height, frame_width, _ = frame.shape
+
+        frame = cv2.line(frame, (1000, 2100), (2800, 230), color=(255, 0, 0), thickness=9)
+        frame = cv2.line(frame, (6855, 2800), (5280, 230), color=(255, 0, 0), thickness=9)
+
+        bg_x = frame_width - 1500 # bg_width
+        bg_y = 0
+
+        left_text = None
+        right_text = None
+
+        if frame_counter > 39 * fps:
+            d = dataframe[['Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4']].values[176:][seconds_counter // fps]
+            
+            left_text = int(str(d[1]).split('.')[0])
+            right_text = int(str(d[2]).split('.')[0])
+
+            seconds_counter += 1
+
+        frame[bg_y:bg_y + 500, bg_x:bg_x + 1500] = make_stats(left_text if left_text else None, right_text if right_text else None)
         cv2.imshow('Frame', frame)
         # frame_filename = f"frame_5_test.jpg"
 
@@ -77,7 +123,7 @@ while cap.isOpened():
         # Break the loop if no frame is read
         break
 
-    # break
+    frame_counter += 1
 
 # Release the video capture object and close all windows
 cap.release()
